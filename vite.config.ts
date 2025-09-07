d1abb2e63c4b1c9f9b7bd75e2ecd6030d99b0c93
jbandu <jbandu@gmail.com>
2025-09-06 21:36:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from 'lovable-tagger'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+// inside defineConfig(...)
+base: process.env.GITHUB_PAGES === 'true' ? '/ujjwaldeep-path-to-victory/' : '/',
+
   server: {
     host: '::',
     port: 8080,
