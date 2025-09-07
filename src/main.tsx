0a13527e1fb77a09ff97e7cd7c345aaf036a2391
jbandu <jbandu@gmail.com>
2025-09-06 22:24:09 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-// import './index.css' // uncomment if you have global styles
+// import './index.css' // if you have global styles
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
