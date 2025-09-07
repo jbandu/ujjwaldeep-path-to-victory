def597738c4f54c342a9a8caa3d6de0e77c11ee9
jbandu <jbandu@gmail.com>
2025-09-06 21:28:03 -0500

    Update basic.spec.ts

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
 test('home page has expected title', async ({ page }) => {
-  await page.goto('/')
+  await page.goto('/')          // ← works now because baseURL is set
   await expect(page).toHaveTitle(/UjjwalDeep/)
 })
