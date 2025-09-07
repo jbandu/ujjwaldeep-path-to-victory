5ecbd7971d5b1bf1572bf6fb01127476f79385e9
jbandu <jbandu@gmail.com>
2025-09-06 21:45:54 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  // playwright.config.ts
+use: { trace: process.env.CI ? 'retain-on-failure' : 'on-first-retry' }
+
   testDir: 'e2e',
   use: {
     baseURL: 'http://localhost:5173',
