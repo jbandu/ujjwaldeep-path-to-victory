ebbd6a6fd066b998a46b35947992983a26031ab2
jbandu <jbandu@gmail.com>
2025-09-06 22:20:28 -0500

    hopefully final test for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,14 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import { HashRouter } from 'react-router-dom'
-
-
-// ...
-<BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App />
-</BrowserRouter>
+import App from './App'
+// import './index.css' // uncomment if you have global styles
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
-// ...
-<React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-</React.StrictMode>
