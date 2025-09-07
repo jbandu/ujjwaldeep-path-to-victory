8238d3169db49269a0d04a7cafb8d816ab67c528
jbandu <jbandu@gmail.com>
2025-09-06 22:06:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath, URL } from 'node:url'
 import { componentTagger } from 'lovable-tagger'
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_PAGES === 'true' ? '/ujjwaldeep-path-to-victory/' : '/',
+  base: mode === 'production' ? '/ujjwaldeep-path-to-victory/' : '/',  // <-- here
   server: { host: '::', port: 8080 },
   plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   resolve: {
