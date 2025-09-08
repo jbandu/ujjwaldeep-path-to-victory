b59322e250935bf43fce322e3ec53a2d115ec65e
jbandu <jbandu@gmail.com>
2025-09-08 17:36:37 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import App from './App'
 import './index.css'
 import ErrorBoundary from '@/components/ErrorBoundary'
 
+
+// src/main.tsx
+import './sentry'
+import { initAnalytics } from './analytics'
+initAnalytics()
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
